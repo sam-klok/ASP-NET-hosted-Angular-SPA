690889fd74c418f4a872dd809ddff1ee90419e10
sam-klok <sklok11@gmail.com>
2026-05-29 11:44:10 -0700

    + .bat files

---
+++ git_sam-klok_setup.bat
@@ -0,0 +1,6 @@
+git config --local user.email 
+git config --local user.name 
+git config --local user.email "sklok11@gmail.com"
+git config --local user.name "sam-klok"
+git config --local user.email
+git config --local user.name  
